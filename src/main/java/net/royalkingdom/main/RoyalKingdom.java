deee78e13237897dbc20a1dbdcc85b39a72af78e
Ayley <larshatzky@gmail.com>
2019-06-10 19:52:35 +0200

    Op Command

--- src/main/java/net/royalkingdom/main/RoyalKingdom.java
+++ src/main/java/net/royalkingdom/main/RoyalKingdom.java
@@ -1,8 +1,12 @@
 package net.royalkingdom.main;
 
+import net.royalkingdom.commands.OpCMD;
+import net.royalkingdom.util.ConfigWerte;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RoyalKingdom extends JavaPlugin {
+
+    private ConfigWerte cw;
 
     @Override
     public void onEnable() {
@@ -15,12 +19,11 @@ public class RoyalKingdom extends JavaPlugin {
     private void registerConfig() {
         getConfig().options().copyDefaults(true);
         saveConfig();
-
-
+        registerCommands();
     }
 
     private void registerCommands(){
-
+        getCommand("op").setExecutor(new OpCMD());
     }
 
     private void registerEvents(){
